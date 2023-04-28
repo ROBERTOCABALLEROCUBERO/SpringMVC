91c5ad077a46f0984eabad38df310046dc4b00a6
Javier Sesma Martinez <me_jsesma@sacyr.com>
2023-04-28 10:01:57 +0200

    Primero

---
+++ MVCCitas/src/main/java/com/example/MVCCitas/MvcCitasApplication.java
@@ -0,0 +1,13 @@
+package com.example.MVCCitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcCitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcCitasApplication.class, args);
+	}
+
+}
---
+++ MVCCitas/src/test/java/com/example/MVCCitas/MvcCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MVCCitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcCitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
